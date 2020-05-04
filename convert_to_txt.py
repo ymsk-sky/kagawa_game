1b8d2be7201bf2ba3ae7d9bd22404cafcca4bb05
ymsk.sky <ymsk.sky.95@gmail.com>
2020-05-04 09:27:52 +0900

    convert img to txt

--- convert_to_txt.py
+++ convert_to_txt.py
@@ -1,6 +1,7 @@
 import cv2
 import glob
 import pyocr
+from PIL import Image
 
 def main():
     img_path = './imgs/'
@@ -19,13 +20,13 @@ def convert_to_txt(img_files):
         # read as grayscale image
         gray_img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
         # convert to binary image
-        _, binary = cv2.threshold(gray_img, 220, 255, cv2.THRESH_BINARY)
-        cv2.imshow('test', binary)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        _, binary_img = cv2.threshold(gray_img, 220, 255, cv2.THRESH_BINARY)
+
+        txt = tool.image_to_string(image=Image.fromarray(binary_img),
+                                   lang='jpn',
+                                   builder=pyocr.builders.TextBuilder(tesseract_layout=6))
+        print(txt)
         break
-        #txt = tool.image_to_string(image=Image.fromarray(binary), lang='jpn', builder=pyocr.builders.TextBuilder(tesseract_layout=6))
-        #print(txt)
 
 def get_img_files(img_path='./imgs/'):
     return glob.glob(img_path+'*/*.png')
